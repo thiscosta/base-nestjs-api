7b956f68ca3e9c93cdde8ad2da8303e12c731cb6
thiscosta <thiscosta@live.com>
2022-03-22 18:42:01 -0300

    test: updated jest coverage collect files

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,14 @@ module.exports = {
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
-  collectCoverageFrom: ['**/*.(t|j)s'],
+  collectCoverageFrom: [
+    'src/*.(t|j)s',
+    'src/**/*.(t|j)s',
+    '!src/*.module.(t|j)s',
+    '!src/**/*.module.(t|j)s',
+    '!src/main.ts',
+    '!src/config/**',
+  ],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
 };
